ef39c4fa978706ed2f8118dadca9f2c6429ccbae
Cipher <cipher@doma.com>
2026-05-07 19:39:29 +0530

    Added some ui for landing page and about_project

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route('/about_project')
+def about_project():
+    return render_template("about_project.html")
