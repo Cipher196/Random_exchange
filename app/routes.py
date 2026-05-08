c2d41371c60bf0e2350f1be32cc8aeb28dd925a2
Cipher <cipher@doma.com>
2026-05-08 20:08:18 +0530

    Tried to and Registration page

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, flash, url_for
+from app.forms import RegistrationForm
 
 main = Blueprint("main", __name__)
 
-@main.route("/")
+@main.route('/')
 def home():
     return render_template("index.html")
 
 @main.route('/about_project')
 def about_project():
     return render_template("about_project.html")
+
+@main.route('/register', methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        print("validation success")
+        flash('Thank for registration!')
+        return redirect(url_for('main.login'))
+    return render_template('register.html', form=form)
+
+
+@main.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if request.method=='POST' and form.validate():
+        flash('Thank for Login!')
+        return redirect(url_for(main.about_project))
+    return render_template('login.html', form=form)
+
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SECRET_KEY = "secret-key"
+    SECRET_KEY = '6c4796e072f0630f8cba09f44f53239c'
     SQLALCHEMY_DATABASE_URI = "sqlite:///database/app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
